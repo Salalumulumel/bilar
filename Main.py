0835e5cbc3233154b18d879854b90a6d340bafe4
Samuel <saso1019@storsjogymnasiet.se>
2023-05-08 14:38:18 +0200

    Hej:)

--- Main.py
+++ Main.py
@@ -1,4 +1,27 @@
 import Bilar
 
-Ford_red = Bilar.Bil("Ford","Röd",5)
+looping = True
 
+Ford_red = Bilar.Bil("Ford","Röd",5)
+Volvo_svart = Bilar.Bil("Volvo", "Svart", 23)
+Volvo_vit = Bilar.Bil("Volvo", "Vit", 7)
+
+Billista = {Ford_red,Volvo_svart,Volvo_vit}
+
+
+
+while looping:
+    print("-----------------------------")
+    print("BilAutomat")
+    
+    i = 0
+
+    for Bil in Billista:
+
+        print(str(i+1) + " : " + Bil.Fabrikat + "  : " + Bil)
+        i +=1
+
+
+    svar = input("\n vill du avsluta programmet? j/n?")
+    if (svar == "j"):
+        break
